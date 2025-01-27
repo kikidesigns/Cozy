cac8b5c46271d043c92c2fd9922a3e7ef0b11c61
Maria Garcia <79329688+kikidesigns@users.noreply.github.com>
2025-01-27 09:21:16 -0600

    Create file app/(tabs)/welcome.tsx

---
+++ app/(tabs)/welcome.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import { Link } from 'expo-router';
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <Link href="/explore" style={styles.link}>
+        <Text style={styles.linkText}>Go to explore page</Text>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
