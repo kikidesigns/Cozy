df0d7e5a2a4b11706efaf390dd7bcf815ca39e9b
Maria Garcia <79329688+kikidesigns@users.noreply.github.com>
2025-01-27 19:00:13 -0600

    Update Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,4 +2,14 @@ export const Colors = {
   platform: '#90EE90', // cozy green
   background: '#87CEEB', // cozy blue
   sun: '#FFFFFF', // white
+  light: {
+    icon: '#000000',
+    text: '#000000',
+    background: '#FFFFFF',
+  },
+  dark: {
+    icon: '#FFFFFF',
+    text: '#FFFFFF',
+    background: '#000000',
+  }
 };
