a78977d54572e1abdd9a3eaa483eca67243d68dc
Maria Garcia <79329688+kikidesigns@users.noreply.github.com>
2025-02-02 10:25:20 -0600

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { globalStyles } from '../../constants/styles';
 
 export default function HomeScreen() {
